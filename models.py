7f3a7ce88fbaad0da96951429fb4417e2769651e
riitikiitkgp <95905958+riitikiitkgp@users.noreply.github.com>
2024-05-19 19:55:44 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    company_name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    state = db.Column(db.String(50), nullable=False)
+    zip = db.Column(db.Integer, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    web = db.Column(db.String(100), nullable=False)
